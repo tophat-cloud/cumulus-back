9c4dcd7d1f79e9ec47dd4ab9e82af8be7c2e813c
ChangHoon Sung <changhoon.dev@gmail.com>
2021-08-31 15:00:36 +0000

    Add restframework

--- rest_server/rest_server/urls.py
+++ rest_server/rest_server/urls.py
@@ -14,35 +14,32 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, re_path
+from django.conf import settings
 from django.conf.urls import url, include
 from rest_framework import routers, permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-# import cumulus.api
-
-# app_name = 'cumulus'
-
-# router = routers.DefaultRouter()
-# router.register('members', cumulus.api.MemberViewSet)
-
-# schema_urlpatterns = [
-#     path('api/', include('cumulus.api')),
-# ]
-# schema_view = get_schema_view(
-#     openapi.Info(
-#         title="Cumulus API",
-#         default_version='v1',
-#         terms_of_service="https://www.google.com/policies/terms/",
-#     ),
-#     public=True,
-#     permission_classes=(permissions.AllowAny,),
-#     patterns=schema_urlpatterns,
-# )
-
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Cumulus API",
+        default_version='v1',
+        terms_of_service="https://www.google.com/policies/terms/",
+    ),
+    public=True,
+    permission_classes=(permissions.IsAuthenticated,),
+)
 
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path(r'api/', include('cumulus.urls')),
+    path(r'account/', include('account.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += [
+        re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name="schema-json"),
+        re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+        re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    ]
