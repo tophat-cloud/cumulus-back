32239db9da4f7ae3be0c33b7a93eafc95cb40876
Jinui You <baram991103@gmail.com>
2021-10-19 14:53:13 +0900

    :sparkles: Add registration API

--- rest_server/rest_server/urls.py
+++ rest_server/rest_server/urls.py
@@ -34,8 +34,9 @@ schema_view = get_schema_view(
 urlpatterns = [
     path(r'admin/', admin.site.urls),
     path('rest-auth/', include('rest_auth.urls')),
+    path(r'^rest-auth/registration/', include('rest_auth.registration.urls')),
     path(r'', include('cumulus.urls')),
-    path(r'accounts/', include('accounts.urls')),
+    # path(r'accounts/', include('accounts.urls')),
 ]
 
 if settings.DEBUG:
