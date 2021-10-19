92e853e74258a129f2aebe2b892dc54dddad4f07
Jinui You <baram991103@gmail.com>
2021-10-19 15:21:27 +0900

    :wrench: Change language code

--- rest_server/rest_server/settings.py
+++ rest_server/rest_server/settings.py
@@ -123,7 +123,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'ko-kr'
+LANGUAGE_CODE = 'en-us'
 
 TIME_ZONE = 'Asia/Seoul'
 
